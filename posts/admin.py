58b827e45b8f9b0b496573b75ddd35b0c1066bc2
Nurbolottop <erk1nbaevw2711@gmail.com>
2022-11-30 16:36:57 +0600

    mather-father

---
+++ posts/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from posts.models import Post, PostLike, PostComment
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(PostLike)
+admin.site.register(PostComment)
